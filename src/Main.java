a2a4a73e12626387ad3549c2c29a5363357f0bb7
Rafael Rodriguez <rerodriguezmun@unal.edu.co>
2022-11-15 22:05:01 -0500

    added: Random number function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Random;
+
 public class Main {
+
+    public static String randomNumber(String in) {
+        String number = "";
+        Random random = new Random();
+        if (in == "TipoA") {
+            number += "54";
+            for (int i = 1; i <= 8; i++)
+                number += Integer.toString(random.nextInt(10));
+        } else {
+            number += "08";
+            for (int i = 1; i <= 8; i++)
+                number += Integer.toString(random.nextInt(10));
+        }
+        return number;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(randomNumber("TipoB"));
     }
 }
